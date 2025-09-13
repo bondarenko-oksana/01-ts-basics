25b94dc90f123951c385ecd55ec4828b70f7fe76
Oksana <familianamy@gmail.com>
2025-09-13 12:26:09 +0300

    first commit

---
+++ src/task-6.ts
@@ -0,0 +1,10 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]);           
+getFirstElement<string>(["a", "b", "c"]);     
+getFirstElement<boolean>([true, false, true]); 
+
+//Перевірка:
+//getFirstElement<number>([5, "ten"]);
